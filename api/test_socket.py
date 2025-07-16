a72b35fd9aa4030efafc6c5718e1d51e0bc89811
Paweł Pietras <pawel.pietras144@gmail.com>
2025-07-16 17:31:33 +0200

    traceback

--- api/test_socket.py
+++ api/test_socket.py
@@ -1,4 +1,5 @@
 import socket
+import traceback
 
 def test_ftp_connection(host, port=21):
     try:
@@ -6,6 +7,7 @@ def test_ftp_connection(host, port=21):
         sock.close()
         return {"status": "Połączenie powiodło się"}
     except Exception as e:
+        print(traceback.format_exc())
         return {"status": f"Błąd połączenia: {e}"}
 
 
